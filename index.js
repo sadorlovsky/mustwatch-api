959d44ad1c55042c629f7f6982e241da1f2beb1c
Zach Orlovsky <sadorlovsky@gmail.com>
2018-01-07 02:16:58 +0300

    lowercase

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const onlyGet = require('micro-get')
 const jwt = require('micro-jwt-auth')
 const got = require('got')
 
-module.exports = jwt(process.env.SECRET)(onlyGet(router(
+module.exports = jwt(process.env.secret)(onlyGet(router(
   get('/', async (req, res) => {
 
-    const response = await got(`${process.env.THEMOVIEDB_API_URL}/search/movie`, {
+    const response = await got(`${process.env.themoviedb_api_url}/search/movie`, {
       json: true,
       query: {
-        api_key: process.env.THEMOVIEDB_API_KEY,
+        api_key: process.env.themoviedb_api_key,
         ...req.query
       }
     })
